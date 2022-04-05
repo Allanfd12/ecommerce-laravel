1a8dab91d342f800e7d5784089a293845cda88b6
allan frança dutram <allanfd12@gmail.com>
2022-04-04 21:05:20 -0300

    add jetstream com login e cadastro de contas

--- resources/views/layouts/padrao.blade.php
+++ resources/views/layouts/padrao.blade.php
@@ -42,8 +42,7 @@
 						</div>
 						<div class="topbar-menu right-menu">
 							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
+
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="{{ asset('assets/images/lang-en.png')}}" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -53,20 +52,33 @@
 										<li class="menu-item" ><a title="canada" href="#"><span class="img label-before"><img src="{{ asset('assets/images/lang-can.png')}}" alt="lang-can"></span>Canada</a></li>
 									</ul>
 								</li>
-								<li class="menu-item menu-item-has-children parent" >
-									<a title="Dollar (USD)" href="#">Dollar (USD)<i class="fa fa-angle-down" aria-hidden="true"></i></a>
-									<ul class="submenu curency" >
-										<li class="menu-item" >
-											<a title="Pound (GBP)" href="#">Pound (GBP)</a>
+							@if (Route::has('login'))
+								@auth
+									@if (Auth::user()->user_type === 'admin')
+										<li class="menu-item menu-item-has-children parent" >
+											<a title=">My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<ul class="submenu curency" >
+												<li class="menu-item" >
+													<a title="Dashboard" href="#">Dashboard</a>
+												</li>
+											</ul>
 										</li>
-										<li class="menu-item" >
-											<a title="Euro (EUR)" href="#">Euro (EUR)</a>
-										</li>
-										<li class="menu-item" >
-											<a title="Dollar (USD)" href="#">Dollar (USD)</a>
-										</li>
-									</ul>
-								</li>
+									@else
+									<li class="menu-item menu-item-has-children parent" >
+										<a title=">My Account" href="#">My Account ok ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+										<ul class="submenu curency" >
+											<li class="menu-item" >
+												<a title="Dashboard" href="#">Dashboard</a>
+											</li>
+										</ul>
+									</li>
+									@endif
+
+								@else
+								<li class="menu-item" ><a title="Register or Login" href="{{route('login')}}">Login</a></li>
+								<li class="menu-item" ><a title="Register or Login" href="{{route('register')}}">Register</a></li>
+								@endauth
+							@endif
 							</ul>
 						</div>
 					</div>
