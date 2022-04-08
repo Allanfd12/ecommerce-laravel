9d8708fa1815d41acb4069dfd40824569cf4df2d
allan frança dutra <allanfd12@gmail.com>
2022-04-08 07:59:00 -0300

    mudando o layout das telas de login e registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -57,4 +57,61 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<x-guest-layout>
+    <main id="main" class="main-site left-sidebar">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="#" class="link">home</a></li>
+					<li class="item-link"><span>Register</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">							
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">
+							<div class="register-form form-item ">
+
+                                <x-jet-validation-errors class="mb-4" />
+                                <form class="form-stl" name="frm-login" method="POST" action="{{ route('register') }}">
+                                    @csrf
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Create an account</h3>
+										<h4 class="form-subtitle">Personal infomation</h4>
+									</fieldset>									
+									<fieldset class="wrap-input">
+										<label for="frm-reg-lname">Nome*</label>
+										<input id="frm-reg-lname" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Seu Nome">
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-reg-email">Endereço de Email*</label>
+										<input type="email" id="frm-reg-email" name="email" :value="old('email')" required  placeholder="Endereço de Email">
+									</fieldset>
+
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Informações de Login</h3>
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half left-item ">
+										<label for="frm-reg-pass">Senha *</label>
+										<input id="frm-reg-pass" type="password" name="password" required autocomplete="new-password" placeholder="Senha">
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half ">
+										<label for="frm-reg-cfpass">Confirmação de Senha *</label>
+										<input type="text" id="frm-reg-cfpass" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmação de Senha">
+									</fieldset>
+									<input type="submit" class="btn btn-sign" value="Register" name="register">
+								</form>
+							</div>											
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main>
+</x-guest-layout>   
--- resources/views/layouts/padrao.blade.php
+++ resources/views/layouts/padrao.blade.php
@@ -56,20 +56,32 @@
 								@auth
 									@if (Auth::user()->user_type === 'admin')
 										<li class="menu-item menu-item-has-children parent" >
-											<a title=">My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<a title=">My Account" href="#">My Admin Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 											<ul class="submenu curency" >
 												<li class="menu-item" >
-													<a title="Dashboard" href="#">Dashboard</a>
+													<a title="Dashboard" href="{{route('admin.dashboard')}}">Dashboard</a>
 												</li>
+												<form id="myFormLogout" method="POST" action="{{route('logout')}}">
+													@csrf
+												<li class="menu-item" >
+													<a title="Sair" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('myFormLogout').submit();">Sair</a>
+												</li>
+												</form>
 											</ul>
 										</li>
 									@else
 									<li class="menu-item menu-item-has-children parent" >
-										<a title=">My Account" href="#">My Account ok ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+										<a title=">My Account" href="#">My Costumer Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 										<ul class="submenu curency" >
 											<li class="menu-item" >
-												<a title="Dashboard" href="#">Dashboard</a>
+												<a title="Dashboard" href="{{route('user.dashboard')}}">Dashboard</a>
 											</li>
+											<form id="myFormLogout" method="POST" action="{{route('logout')}}">
+												@csrf
+												<li class="menu-item" >
+													<a title="Sair" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('myFormLogout').submit();">Sair</a>
+												</li>
+												</form>
 										</ul>
 									</li>
 									@endif
